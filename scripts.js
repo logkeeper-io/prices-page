b466b29f72c0ac86db71a6221a7d52d194e55f31
Volodymyr Lapada <5500791+serenityzn@users.noreply.github.com>
2024-07-30 13:32:11 +0200

    feat: auto calculate prise based on the retention days

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 // scripts.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    const userDropdown = document.getElementById('user-dropdown');
+    const userDropdown = document.getElementById('retention-dropdown');
     const proPriceElement = document.getElementById('pro-price');
-    const userCountPro = document.getElementById('user-count-pro');
+    const userCountPro = document.getElementById('rotation-count-days');
 
     // Price mapping
     const priceMap = {
-        5: 19,
-        10: 22,
-        15: 30,
-        20: 40
+        3: 0.4,
+        7: 0.6,
+        14: 0.75,
+        30: 0.9
     };
 
     function updatePrice() {
         const selectedUsers = parseInt(userDropdown.value, 10);
         userCountPro.textContent = selectedUsers;
         const price = priceMap[selectedUsers] || 19; // Default price
-        proPriceElement.innerHTML = `$${price}<span>/mo</span>`;
+        proPriceElement.innerHTML = `$${price}<span>/per Gb ingested</span>`;
     }
 
     userDropdown.addEventListener('change', updatePrice);
