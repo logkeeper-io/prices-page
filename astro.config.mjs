83c3688157ea78912097ba3bbac3ee6490c7a0a7
Volodymyr Lapada <5500791+serenityzn@users.noreply.github.com>
2024-09-05 14:27:25 +0200

    feat: fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
-  site: 'https://logkeeper-io.github.io',
+  integrations: [react(), tailwind()],
+  site: 'https://logkeeper-io.github.io'
 });
