5b20d90ae5cc1d69de5578d91ac391349a54a0c4
Volodymyr Lapada <5500791+serenityzn@users.noreply.github.com>
2024-09-05 12:30:52 +0200

    feat: Added more integration logos

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,13 +1,5 @@
 import { motion } from "framer-motion";
-
-import { HttpdLogo } from "../assets/logos/HttpdLogo";
-import { AmazonLogo } from "../assets/logos/AmazonLogo";
 import { AWSLogo } from "../assets/logos/AWSLogo";
-import { DropboxLogo } from "../assets/logos/DropboxLogo";
-import { NetflixLogo } from "../assets/logos/NetflixLogo";
-import { SlackLogo } from "../assets/logos/SlackLogo";
-import { SpotifyLogo } from "../assets/logos/SpotifyLogo";
-import { StripeLogo } from "../assets/logos/StripeLogo";
 import { AzureLogo } from "../assets/logos/AzureLogo";
 import { GCPLogo } from "../assets/logos/GCPLogo";
 import { CassandraLogo } from "../assets/logos/CassandraLogo";
@@ -17,61 +9,67 @@ import { PgsqlLogo } from "../assets/logos/PgsqlLogo";
 import { MongoDbLogo } from "../assets/logos/MongoDbLogo";
 import { RabbitMqLogo } from "../assets/logos/RabbitMqLogo";
 
-export const Brands = () => (
-  <section className="py-12 sm:py-24 bg-bgDark1 w-full  lg:mt-16 mb-8 lg:mb-16">
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.5, delay: 0.2 }}
-    >
-      <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5">
-        <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
-          <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
-            <div className="flex flex-col">
-              <h2 className="mb-2  text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-primaryText">
-                Seamless Integrations
-              </h2>
-              <h2 className=" text-4xl  sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-secondaryColor">
-                with your tools and services.
-              </h2>
-            </div>
+const logos = [
+  <AWSLogo key="AWS" />,
+  <AzureLogo key="Azure" />,
+  <GCPLogo key="GCP" />,
+  <CassandraLogo key="Cassandra" />,
+  <NginxLogo key="Nginx" />,
+  <MysqlLogo key="Mysql" />,
+  <PgsqlLogo key="Pgsql" />,
+  <MongoDbLogo key="MongoDb" />,
+  <RabbitMqLogo key="RabbitMq" />
+];
+
+const renderLogoGrid = () => (
+    <div className="flex flex-wrap">
+      {logos.map((logo) => (
+          <div className="w-1/3 p-1 flex justify-center items-center" key={logo.key}>
+            {logo}
           </div>
-          <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
-            <div className="flex flex-wrap -m-4">
+      ))}
+    </div>
+);
 
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <AWSLogo />
+export const Brands = () => (
+    <section className="py-12 sm:py-24 bg-bgDark1 w-full lg:mt-16 mb-8 lg:mb-16">
+      <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        <div className="container px-4 mx-auto 2xl:w-[1200px] xl:w-[1100px] lg:w-[1000px] md:w-4/5">
+          <div className="flex lg:flex-row flex-col items-center -mx-4 justify-center lg:text-left text-center">
+            <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
+              <div className="flex flex-col">
+                <h2 className="mb-2 text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-primaryText">
+                  Seamless Integrations
+                </h2>
+                <h2 className=" text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-secondaryColor">
+                  with your tools and services.
+                </h2>
               </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <AzureLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <GCPLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <CassandraLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <NginxLogo />
-              </div>
-
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <MysqlLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3  py-6 flex  justify-center">
-                <PgsqlLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3  py-6 flex  justify-center">
-                <MongoDbLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3  py-6 flex  justify-center">
-                <RabbitMqLogo />
+            </div>
+            <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
+              <div className="relative overflow-hidden">
+                <motion.div
+                    className="flex"
+                    animate={{ x: ["0%", "-100%"] }}
+                    transition={{ duration: 30, repeat: Infinity, ease: "linear" }} // Adjust duration for smoothness
+                    style={{ display: 'flex', width: '100%' }} // Ensure width is double the container
+                >
+                  <div className="flex-shrink-0 w-full flex">
+                    {renderLogoGrid()}
+                  </div>
+                  <div className="flex-shrink-0 w-full flex">
+                    {renderLogoGrid()}
+                  </div>
+                </motion.div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </motion.div>
-  </section>
+      </motion.div>
+    </section>
 );
