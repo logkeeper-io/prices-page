d7c2ad87a29bb46865683402e354c73622d90095
Volodymyr Lapada <5500791+serenityzn@users.noreply.github.com>
2024-09-05 17:59:13 +0200

    feat: Added more logos

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -8,6 +8,16 @@ import { MysqlLogo } from "../assets/logos/MysqlLogo";
 import { PgsqlLogo } from "../assets/logos/PgsqlLogo";
 import { MongoDbLogo } from "../assets/logos/MongoDbLogo";
 import { RabbitMqLogo } from "../assets/logos/RabbitMqLogo";
+import { GolangLogo } from "../assets/logos/GolangLogo";
+import { JavaLogo } from "../assets/logos/JavaLogo";
+import { PythonLogo } from "../assets/logos/PythonLogo";
+import { PhpLogo } from "../assets/logos/PhpLogo";
+import { AwsLambdaLogo } from "../assets/logos/AwsLambdaLogo";
+import { EKSLogo } from "../assets/logos/EKSLogo";
+import { RedisLogo } from "../assets/logos/RedisLogo";
+import { AKSLogo } from "../assets/logos/AKSLogo";
+import { GKELogo } from "../assets/logos/GKELogo";
+import { ECSLogo } from "../assets/logos/ECSLogo";
 
 const logos = [
   <AWSLogo key="AWS" />,
@@ -18,13 +28,29 @@ const logos = [
   <MysqlLogo key="Mysql" />,
   <PgsqlLogo key="Pgsql" />,
   <MongoDbLogo key="MongoDb" />,
-  <RabbitMqLogo key="RabbitMq" />
+  <RabbitMqLogo key="RabbitMq" />,
+  <GolangLogo key="Golang" />,
+  <JavaLogo key="Java" />,
+  <PythonLogo key="Python" />,
+  <PhpLogo key="Php" />,
+  <AwsLambdaLogo key="AwsLambda" />,
+  <EKSLogo key="EKS" />,
+  <RedisLogo key="Redis" />,
+  <AKSLogo key="AKS" />,
+  <GKELogo key="GKE" />,
+  <ECSLogo key="ECS" />,
+  // <CassandraLogo key="Cassandra" />,
+  // <NginxLogo key="Nginx" />,
+  // <MysqlLogo key="Mysql" />,
+  // <PgsqlLogo key="Pgsql" />,
+  // <MongoDbLogo key="MongoDb" />,
+  // <RabbitMqLogo key="RabbitMq" />
 ];
 
 const renderLogoGrid = () => (
-    <div className="flex flex-wrap">
+    <div className="grid grid-rows-3 grid-flow-col gap-4">
       {logos.map((logo) => (
-          <div className="w-1/3 p-1 flex justify-center items-center" key={logo.key}>
+          <div className="p-1 flex justify-center items-center" key={logo.key}>
             {logo}
           </div>
       ))}
@@ -57,7 +83,7 @@ export const Brands = () => (
                     className="flex"
                     animate={{ x: ["0%", "-100%"] }}
                     transition={{ duration: 30, repeat: Infinity, ease: "linear" }} // Adjust duration for smoothness
-                    style={{ display: 'flex', width: '100%' }} // Ensure width is double the container
+                    style={{ display: 'flex', width: '128%' }} // Ensure width is double the container
                 >
                   <div className="flex-shrink-0 w-full flex">
                     {renderLogoGrid()}
